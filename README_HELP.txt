a5e9d12ba63f59bd4fa94670640782142587439d
Dmitriy Donskikh <dndonskih@gmail.com>
2022-01-30 13:14:33 +0300

    init

---
+++ README_HELP.txt
@@ -0,0 +1,2 @@
+1)git clone https://github.com/DNDONSKIH/StepikNginx.git
+2)sudo bash /home/box/StepikNginx/init.sh
---
+++ init.sh
@@ -0,0 +1,6 @@
+sudo mv /home/box/StepikNginx/web ~
+sudo rm /etc/nginx/sites-enabled/default
+sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
+sudo /etc/init.d/nginx restart
+sudo ln -sf /home/box/web/etc/gunicorn.conf /etc/gunicorn.d/test
+sudo /etc/init.d/gunicorn restart
